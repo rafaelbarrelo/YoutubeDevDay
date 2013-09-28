2f4210d542a038878b0ab844ef865d1635a89265
Daniel Veronez <dnl.vzlt@gmail.com>
2013-09-28 15:31:00 -0300

    2

--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,21 @@ function YoutubeCTRL($scope, $http) {
 
   $scope.url = "http://gdata.youtube.com/feeds/api/videos?q=dogs&alt=json-in-script&callback=?&max-results=12&start-index=1";
 
+  $scope.data = Array();
 
-  $http.get('https://www.googleapis.com/youtube/v3/videoCategories\\?part=snippet&regionCode=BR&key=AIzaSyDTa1AZ-NHheqfEeVLG350m6u7TqnbB-2E').
-    success(function(data, status, headers, config) {
-      data;
-      // this callback will be called asynchronously
-      // when the response is available
-    }).
-    error(function(data, status, headers, config) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-    });
+  $scope.getData = function() {
+    $http.get('https://www.googleapis.com/youtube/v3/videoCategories\\?part=snippet&regionCode=BR&key=AIzaSyDTa1AZ-NHheqfEeVLG350m6u7TqnbB-2E').
+      success(function(data, status, headers, config) {
+        $scope.data = data.items;
+      }).
+      error(function(data, status, headers, config) {
+      });
+  }
+
+  $scope.pegarMais = function(id) {
+    $scope.obj.items.id
+  }
+
+  $scope.getData();
 
 }
